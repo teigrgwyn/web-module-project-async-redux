62f89106fac208d52e70ae0619249d2acbbe6d48
Cory Young <teigrgwyn@gmail.com>
2021-03-17 12:46:45 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+
+		</div>
+	);
+}
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
+
+export default App;
+
+/*
+	Theme:
+		Bluish: #20232A
+		Dark Bluish: #0D1117
+		Dark Gray: #181A1B
+		Near Black: #0D0D0D
+*/
